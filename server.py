730b4d6841617926d7327c398e0feac5118502ce
po1nt-1 <po1nt@protonmail.ch>
2021-01-06 22:30:52 +1000

    new server

--- server.py
+++ server.py
@@ -1,24 +1,16 @@
 import json
-import threading
-import socketserver
+import socket
 import sys
+import threading
 import time
 
 import psutil
 
+HOST, PORT = '0.0.0.0', 55555
+
 stats = {}
 step = 1
-
-
-class MyTCPHandler(socketserver.BaseRequestHandler):
-    def handle(self):
-        global step
-        global stats
-
-        if step % 2 == 0:
-            self.request.sendall(stats.encode(encoding='utf-8'))
-            print('Sent', stats, f'at {time.strftime("%H:%M:%S")}')
-            time.sleep(0.5)
+clients = {}
 
 
 def monitor(period):
@@ -30,41 +22,75 @@ def monitor(period):
         while True:
             if time.time() - time_ > period:
                 time_ = time.time()
-                step += 1
 
                 print('Monitoring..')
                 cpu = psutil.cpu_percent(0.1)
                 r = psutil.virtual_memory()
                 data = {'cpu': cpu, 'ram': [r.percent, r.used / (1024**3)]}
 
-                stats = json.dumps(data)
+                stats = json.dumps(data).encode(encoding='utf-8')
+
+                step += 1
+    except KeyboardInterrupt:
+        return -1
+
+
+def sender():
+    try:
+        global stats
+        global clients
+        global step
+
+        while True:
+            if step % 2 == 0 and stats and clients:
+                kick_list = []
+                for addr, conn in clients.items():
+                    try:
+                        conn.sendall(stats)
+                    except BrokenPipeError:
+                        kick_list.append(addr)
+
+                for ip in kick_list:
+                    clients.pop(ip)
+
+                stats = {}
     except KeyboardInterrupt:
         return -1
 
 
 def main():
     try:
-        HOST, PORT = '0.0.0.0', 55555
+        global clients
 
-        try:
-            arg = sys.argv[1]
-            if not arg or not arg.isnumeric():
+        arg = sys.argv[1]
+        if not arg or not arg.isnumeric():
+            raise IndexError
+        else:
+            period = int(arg)
+            if not period > 0:
                 raise IndexError
-            else:
-                period = int(arg)
-                if not period > 0:
-                    raise IndexError
-        except IndexError:
-            print('Specify the period in seconds as the parameter!\n'
-                  'Like this: python server.py 5')
-            return -1
 
         thread = threading.Thread(target=monitor, args=(period,))
         thread.setDaemon(True)
         thread.start()
 
-        with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
-            server.serve_forever()
+        thread = threading.Thread(target=sender)
+        thread.setDaemon(True)
+        thread.start()
+
+        clients = {}
+        server = socket.socket()
+        server.bind((HOST, PORT))
+        server.listen()
+
+        while True:
+            conn, addr = server.accept()
+            if f'{addr[0]}:{addr[1]}' not in clients.keys():
+                clients.update({f'{addr[0]}:{addr[1]}': conn})
+
+    except IndexError:
+        print('Specify the period in seconds as the parameter!\n'
+              'Like this: python server.py 5')
     except KeyboardInterrupt:
         pass
 
