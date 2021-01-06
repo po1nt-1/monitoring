6278e5cb4e2c463a775a52f80dd8d93cdbaf2dc3
po1nt-1 <po1nt@protonmail.ch>
2021-01-06 22:57:12 +1000

    add client reconnection

--- client.py
+++ client.py
@@ -4,16 +4,22 @@ import time
 
 if __name__ == "__main__":
     HOST, PORT = input('HOST: '), 55555
+    TIMEOUT = 9
 
     while True:
         try:
-            test_val = 5
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             sock.connect((HOST, PORT))
+            time_ = time.time()
             while True:
                 stats = sock.recv(64).decode(encoding='utf-8')
                 if stats:
+
                     print(json.loads(stats))
+
+                    time_ = time.time()
+                if time.time() - time_ > TIMEOUT:
+                    break
         except ConnectionResetError:
             sock.close()
             print('Connection Reset Error')
