83d8ec94bd6145e6045b64fa870a0610aaa4974f
po1nt-1 <po1nt@protonmail.ch>
2021-01-06 22:06:54 +1000

    new client

--- client.py
+++ client.py
@@ -1,12 +1,26 @@
 import json
 import socket
+import time
 
 if __name__ == "__main__":
     HOST, PORT = input('HOST: '), 55555
 
     while True:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        try:
+            test_val = 5
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             sock.connect((HOST, PORT))
-            stats = sock.recv(64).decode(encoding='utf-8')
-            if stats:
-                print(json.loads(stats))
+            while True:
+                stats = sock.recv(64).decode(encoding='utf-8')
+                if stats:
+                    print(json.loads(stats))
+        except ConnectionResetError:
+            sock.close()
+            print('Connection Reset Error')
+            continue
+        except ConnectionRefusedError:
+            sock.close()
+            print('Connection Refused Error')
+            continue
+        finally:
+            time.sleep(1)
--- server.py
+++ server.py
@@ -1,49 +1,72 @@
 import json
+import threading
 import socketserver
+import sys
 import time
 
 import psutil
 
 stats = {}
 step = 1
-time_ = 0
-period = 5
 
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
     def handle(self):
-        global time_
         global step
         global stats
 
-        if time.time() - time_ > period:
-            print('Monitoring..')
-            stats = monitor()
-            time_ = time.time()
-
-            step += 1
-            if step % 2 == 0:
-                self.request.sendall(stats.encode(encoding='utf-8'))
-                print('Sent', stats, f'at {time.strftime("%H:%M:%S")}')
+        if step % 2 == 0:
+            self.request.sendall(stats.encode(encoding='utf-8'))
+            print('Sent', stats, f'at {time.strftime("%H:%M:%S")}')
+            time.sleep(0.5)
 
 
-def monitor():
-    cpu = psutil.cpu_percent(0.1)
-    r = psutil.virtual_memory()
-    data = {'cpu': cpu, 'ram': [r.percent, r.used / (1024**3)]}
-    return json.dumps(data)
+def monitor(period):
+    try:
+        global stats
+        global step
+
+        time_ = 0
+        while True:
+            if time.time() - time_ > period:
+                time_ = time.time()
+                step += 1
+
+                print('Monitoring..')
+                cpu = psutil.cpu_percent(0.1)
+                r = psutil.virtual_memory()
+                data = {'cpu': cpu, 'ram': [r.percent, r.used / (1024**3)]}
+
+                stats = json.dumps(data)
+    except KeyboardInterrupt:
+        return -1
 
 
 def main():
-    HOST, PORT = '0.0.0.0', 55555
-    global period
-    global stats
+    try:
+        HOST, PORT = '0.0.0.0', 55555
 
-    period = int(input('Enter the monitoring period in seconds: '))
+        try:
+            arg = sys.argv[1]
+            if not arg or not arg.isnumeric():
+                raise IndexError
+            else:
+                period = int(arg)
+                if not period > 0:
+                    raise IndexError
+        except IndexError:
+            print('Specify the period in seconds as the parameter!\n'
+                  'Like this: python server.py 5')
+            return -1
 
-    stats = monitor()
-    with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
-        server.serve_forever()
+        thread = threading.Thread(target=monitor, args=(period,))
+        thread.setDaemon(True)
+        thread.start()
+
+        with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
+            server.serve_forever()
+    except KeyboardInterrupt:
+        pass
 
 
 if __name__ == "__main__":
